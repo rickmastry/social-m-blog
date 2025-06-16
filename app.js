cedd7ded059663dab0cb9a33b42b3d0f2496112d
Rick Mastry <jahrik@hotmail.com>
2025-06-16 17:34:58 -0400

    Chat feature added

--- app.js
+++ app.js
@@ -49,5 +49,22 @@ app.set('views', 'views')
 app.set('view engine', 'ejs')
 app.use('/', router)
 
+const server = require('http').createServer(app)
 
-module.exports = app
+const io = require('socket.io')(server)
+
+io.use(function (socket, next) {
+  sessionOptions(socket.request, socket.request.res || {}, next)
+})
+
+io.on('connection', function(socket) {
+ if (socket.request.session.user) {
+     let user = socket.request.session.user
+     socket.emit('Welcome', {username: user.username, avatar: user.avatar})
+     socket.on('chatMessageFromBrowser', function(data){
+       socket.broadcast.emit('chatMessageFromServer', {message: sanitizeHTML(data.message, {allowedTags: [], allowedAttributes: {}}), username: user.username, avatar: user.avatar})
+ })
+ }
+})
+
+module.exports = server
